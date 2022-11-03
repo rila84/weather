ffcb577b5e1b7e9129a5bb92654863bafa56122a
rila <raheleh.fotouhi@gmail.com>
2022-11-03 17:42:36 +0330

    js added

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
+function showTemperature(response) {
+console.log(response.data.name);
+  
+}
+
 let apiKey = "5d9235a86e48ae6996d42d29c5604b9e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
