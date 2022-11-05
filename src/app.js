4707850772927b0ec5621fe8c32df2857db31b25
rila <raheleh.fotouhi@gmail.com>
2022-11-05 12:49:55 +0330

    displaying the current temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
-function showTemperature(response) {
-console.log(response.data.name);
-  
+function showWeatherInformation(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
-
-let apiKey = "5d9235a86e48ae6996d42d29c5604b9e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+function searchCity(city) {
+  let apiKey = "5d9235a86e48ae6996d42d29c5604b9e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherInformation);
+}
+function submitting(event) {
+  event.preventDefault();
+  let city = document.querySelector("#current-city");
+  let cityInput = document.querySelector("#search-input").value;
+  city = cityInput;
+  searchCity(city);
+}
+let submitCity = document.querySelector("#search-form");
+submitCity.addEventListener("submit", submitting);
