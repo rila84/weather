2310028dcf9e72102d28c08326fb3125304ca0e3
rila <raheleh.fotouhi@gmail.com>
2022-11-05 14:48:14 +0330

    date updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function date(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
+}
+
 function showWeatherInformation(response) {
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
@@ -11,12 +34,14 @@ function showWeatherInformation(response) {
   document.querySelector("#wind-speed").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#date").innerHTML = date(response.data.dt * 1000);
 }
 function searchCity(city) {
   let apiKey = "5d9235a86e48ae6996d42d29c5604b9e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeatherInformation);
 }
+
 function submitting(event) {
   event.preventDefault();
   let city = document.querySelector("#current-city");
